9e56f0692862d264878f113ad87d983d2421af43
Amadeusz Wieczorek <git@amadeusw.com>
2019-01-03 17:02:51 -0800

    moving enemy

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float Radius;
+    public float SpawnDistance;
+    public float Speed;
+    public float ApproachSpeed;
+    public float PhaseShift;
+
+    private float SpawnTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t = Time.time - SpawnTime;
+        var x = Mathf.Sin(t * Speed + PhaseShift) * Radius;
+        var z = Mathf.Cos(t * Speed + PhaseShift) * Radius;
+        var y = SpawnDistance - t * ApproachSpeed;
+        transform.position = new Vector3(x, y, z);
+    }
+}
